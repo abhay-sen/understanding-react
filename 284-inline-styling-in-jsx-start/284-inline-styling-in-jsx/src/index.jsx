69690cbb96d89f0b5187fbcd2c9675d171908543
abhay-sen <abhaysen0502@gmail.com>
2024-03-14 22:04:18 +0530

    as

--- 284-inline-styling-in-jsx-start/284-inline-styling-in-jsx/src/index.jsx
+++ 284-inline-styling-in-jsx-start/284-inline-styling-in-jsx/src/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const customStyle={
+    color : "red",
+    fontSize : "60px",
+}
+ReactDOM.render(<h1 style={customStyle}>Hello World!</h1>, document.getElementById("root"));
 
 // If you're running this locally in VS Code use the commands:
 // npm install
 // to install the node modules and
 // npm run dev
 // to launch your react project in your browser
+// the double curly braces is one object as a js inside a html

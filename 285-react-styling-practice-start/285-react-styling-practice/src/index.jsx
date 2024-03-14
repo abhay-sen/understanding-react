ee37e5a6b9d7f67473c18b176e9cd2104e115af3
abhay-sen <abhaysen0502@gmail.com>
2024-03-14 22:14:51 +0530

    yuop

--- 285-react-styling-practice-start/285-react-styling-practice/src/index.jsx
+++ 285-react-styling-practice-start/285-react-styling-practice/src/index.jsx
@@ -11,3 +11,27 @@
 // to install the node modules and
 // npm run dev
 // to launch your react project in your browser
+import React from 'react';
+import ReactDOM from 'react-dom';
+const inlineStyle={
+    color : 'red',
+}
+const  date = new Date();
+const time= date.getHours();
+let greeting;
+if(time>=0&& time<12) {
+    greeting="Good Morning";
+    inlineStyle.color='red';
+}
+else if(time>=12&&time<18) {
+    greeting="Good Afternoon";
+    inlineStyle.color='green';
+}
+else {
+    greeting="Good Evening";
+    inlineStyle.color='blue';
+}
+console.log(time);
+ReactDOM.render(<div>
+    <h1 style={inlineStyle}> {greeting}</h1>
+</div>, document.getElementById('root'));

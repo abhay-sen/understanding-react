756b28ed2cf1de7d53b3566cf29c4206e5918cda
abhay-sen <abhaysen0502@gmail.com>
2024-03-19 19:55:41 +0530

    hmm

---
+++ 286-react-components-start/286-react-components/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./Heading.jsx"
+import List from "./list.jsx"
+
+function App() {
+    return <div>
+        <Heading/>
+        <List/>
+        <List/>
+        <Heading/>
+    </div>
+}
+
+export default App;
--- 287-react-components-practice-start/287-react-components-practice/src/index.jsx
+++ 287-react-components-practice-start/287-react-components-practice/src/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from  "./components/App";
+import App from "./App.jsx"
 
 ReactDOM.render(
-  <App/>,document.getElementById("root")
+  <App/>,
+  document.getElementById("root")
 );
 
 // If you're running this locally in VS Code use the commands:
